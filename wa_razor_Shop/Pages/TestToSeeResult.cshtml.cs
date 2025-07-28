e1ff6d61141a225d72bb4a035b4c5dd764d5a545
Alireza Seyfi <apple1music.com@gmail.com>
2025-07-28 15:03:51 +0330

    Add New Page For New Branch

---
+++ wa_razor_Shop/Pages/TestToSeeResult.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace wa_razor_Shop.Pages
+{
+    public class TestToSeeResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
